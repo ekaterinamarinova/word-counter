c967796663eedc924b89fa84e1fa9a00212e8e92
Mladen Ivanov <mladen.ivanov@scalefocus.com>
2019-11-18 11:47:54 +0200

    Add bare bones of the app

---
+++ src/main/java/com/personaltask/wordcounter/WordCounterApp.java
@@ -0,0 +1,15 @@
+package com.personaltask.wordcounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Mladen Ivanov
+ */
+@SpringBootApplication
+public class WordCounterApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WordCounterApp.class, args);
+    }
+}
