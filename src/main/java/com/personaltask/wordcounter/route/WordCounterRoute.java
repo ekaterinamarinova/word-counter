a295df1b84a424e566e028cbbc68fbbd3472c824
ekaterina.marinova <ekaterina.marinova@scalefocus.com>
2020-02-07 18:30:40 +0200

    -error handling improvements

--- src/main/java/com/personaltask/wordcounter/route/WordCounterRoute.java
+++ src/main/java/com/personaltask/wordcounter/route/WordCounterRoute.java
@@ -3,10 +3,11 @@ package com.personaltask.wordcounter.route;
 import com.personaltask.wordcounter.exception.*;
 import com.personaltask.wordcounter.processor.*;
 import com.personaltask.wordcounter.property.yml.ApplicationProperties;
-import com.personaltask.wordcounter.property.yml.GoogleCloudProperties;
 import org.apache.camel.builder.RouteBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -22,17 +23,18 @@ public class WordCounterRoute extends RouteBuilder {
     private static final Logger LOGGER = LoggerFactory.getLogger(WordCounterRoute.class);
 
     private ApplicationProperties applicationProperties;
-    private GoogleCloudProperties googleCloudProperties;
+    private ConfigurableApplicationContext context;
 
-    public WordCounterRoute(ApplicationProperties applicationProperties, GoogleCloudProperties googleCloudProperties) {
+    @Autowired
+    public WordCounterRoute(ApplicationProperties applicationProperties, ConfigurableApplicationContext context) {
         this.applicationProperties = applicationProperties;
-        this.googleCloudProperties = googleCloudProperties;
+        this.context = context;
     }
 
     public void configure() throws Exception {
-
         onException(NoSuchBucketException.class)
-                .process(ExceptionLoggingProcessor.NAME);
+                .process(ExceptionLoggingProcessor.NAME)
+                .process(exchange -> stop());
 
         onException(BlobNotFoundException.class)
                 .handled(true)
@@ -51,6 +53,9 @@ public class WordCounterRoute extends RouteBuilder {
                 .handled(true)
                 .process(ExceptionLoggingProcessor.NAME);
 
+        onCompletion()
+                .process(CleanLocalDirProcessor.NAME);
+
         from("quartz2://simpleCron?cron=" + applicationProperties.getCronExpressionWorkdaysEachMinute())
                 .process(DownloadProcessor.NAME)
                 .split(body())
@@ -58,5 +63,14 @@ public class WordCounterRoute extends RouteBuilder {
                     .process(UploadProcessor.NAME)
                     .process(MoveProcessor.NAME)
                 .end();
+    }
+
+    public void stop() throws Exception {
+        getContext().getShutdownStrategy().setTimeout(1);
+        getContext().stop();
+
+        context.close();
+
+        System.exit(1);
     }
 }
