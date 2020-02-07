b4dceb8635c732efac4e7d9ab027817f4e4ddd2a
ekaterina.marinova <ekaterina.marinova@scalefocus.com>
2020-02-07 18:30:06 +0200

    -added configuration for quartz component

---
+++ src/main/java/com/personaltask/wordcounter/configuration/CamelConfiguration.java
@@ -0,0 +1,17 @@
+package com.personaltask.wordcounter.configuration;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.component.quartz2.QuartzComponent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CamelConfiguration {
+
+    @Bean
+    public QuartzComponent quartzComponent(CamelContext camelContext) {
+        QuartzComponent quartz2 = camelContext.getComponent("quartz2", QuartzComponent.class);
+        quartz2.setInterruptJobsOnShutdown(true);
+        return quartz2;
+    }
+}
